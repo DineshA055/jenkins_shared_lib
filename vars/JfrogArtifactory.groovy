0f7fbc0aa7ae08d96a99f1cdbc5aff8b9c7cd196
DineshA <101075223+DineshA055@users.noreply.github.com>
2024-02-07 16:42:04 +0530

    Update JfrogArtifactory.groovy

--- vars/JfrogArtifactory.groovy
+++ vars/JfrogArtifactory.groovy
@@ -4,9 +4,8 @@ def call(credentialsId){
             credentialsId: "Jfrog-api",
             usernameVariable: "Username,
             passwordVariable: "Password"
-    )]) {
-             sh 'curl -X PUT -u $username -p $password -T /var/lib/jenkins/workspace/jfrog_test/target/*.jar http://3.109.184.227:8082/artifactory/example-repo-local/'
-     }
+    )]) 
+            
 }
     
 //Username
