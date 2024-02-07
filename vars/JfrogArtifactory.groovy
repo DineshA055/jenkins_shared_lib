4a3b784558433712f3346deac1b33cf498533b15
DineshA <101075223+DineshA055@users.noreply.github.com>
2024-02-07 15:42:46 +0530

    Update JfrogArtifactory.groovy

--- vars/JfrogArtifactory.groovy
+++ vars/JfrogArtifactory.groovy
@@ -5,7 +5,7 @@ def call(credentialsId){
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {{
-      sh 'curl -X PUT -u $USER $PASS -T /var/lib/jenkins/workspace/jfrog_test/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://3.109.184.227:8082/artifactory/example-repo-local/'
+      sh 'curl -X PUT -u $USER -p $PASS -T /var/lib/jenkins/workspace/jfrog_test/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://3.109.184.227:8082/artifactory/example-repo-local/'
     }
 }
 //Username
