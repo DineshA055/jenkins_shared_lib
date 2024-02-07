432dc8d1493109bf1113abec70698b4b790ee4ab
DineshA <101075223+DineshA055@users.noreply.github.com>
2024-02-07 19:44:41 +0530

    Update JfrogArtifactory.groovy

--- vars/JfrogArtifactory.groovy
+++ vars/JfrogArtifactory.groovy
@@ -22,13 +22,13 @@
 def call(){
     withCredentials([usernamePassword(
             credentialsId: "Artifact",
-            usernameVariable: "USER",
+           usernameVariable: "USER",
             passwordVariable: "PASS"
-    )]) 
+  )]) 
     //sh "docker image push ${hubUser}/${project}:${ImageTag}"
    // sh "docker image push ${hubUser}/${project}:latest"   
-  {
-  'sh curl -X -u ${USER} -p {PASS} -T /var/lib/jenkins/workspace/jfrog_test/target/*.jar http://3.109.184.227:8082/artifactory/example-repo-local/'
-  }
+//  {
+//  'sh curl -X PUT -u ${USER}:${PASS} -T /var/lib/jenkins/workspace/jfrog_test/target/*.jar http://3.109.184.227:8082/artifactory/example-repo-local/'
+  //}
 
 }
