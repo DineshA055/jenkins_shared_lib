ac9897ad08468e417f9da71e791484a0a412dfaf
DineshA <101075223+DineshA055@users.noreply.github.com>
2024-02-07 14:30:06 +0530

    Update JfrogArtifactory.groovy

--- vars/JfrogArtifactory.groovy
+++ vars/JfrogArtifactory.groovy
@@ -1,7 +1,7 @@
 def call(credentialsId){
 
-    withJfrogArtifactoryEnv(credentialsId: credentialsId) {
-      sh 'Curl -X PUT -u $jfrog-api -p $jfrog-api -T *.jar http://3.109.184.227:8081/artifactory/example-repo-local/
+    withJfrogArtifactoryEnv(credentialsId: Jfrog-api) {
+      sh 'Curl -X PUT -u $Username -p $Password -T *.jar http://3.109.184.227:8081/artifactory/example-repo-local/
     }
 }
 //Username
