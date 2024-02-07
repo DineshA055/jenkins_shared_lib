0e19a6c5d36bb43aea06a0c638ed3432b31425bd
DineshA <101075223+DineshA055@users.noreply.github.com>
2024-02-07 17:02:46 +0530

    Update JfrogArtifactory.groovy

--- vars/JfrogArtifactory.groovy
+++ vars/JfrogArtifactory.groovy
@@ -9,11 +9,11 @@
 //}
 
 
-def call(credentialsId){
+//def call(credentialsId){
 
-   withJfrogArtifactoryeEnv(credentialsId: credentialsId) {
-          'sh curl -X -u admin -p -T /var/lib/jenkins/workspace/jfrog_test/target/*.jar http://3.109.184.227:8082/artifactory/example-repo-local/
-   }
-}
+//   withJfrogArtifactoryeEnv(credentialsId: credentialsId) {
+         // 'sh curl -X -u admin -p -T /var/lib/jenkins/workspace/jfrog_test/target/*.jar http://3.109.184.227:8082/artifactory/example-repo-local/
+//   }
+//}
 //Username
 //Password
